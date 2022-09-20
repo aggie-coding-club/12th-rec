7f283ea4d6219c80928c7e4e2aa5c821751f80e6
SumitNalavade <vs.nalavade2003@gmail.com>
2022-09-20 18:08:52 -0500

    Add fonts

--- App.tsx
+++ App.tsx
@@ -1,17 +1,59 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, extendTheme } from "native-base";
 
 import SignInScreen from "./screens/signIn";
 import SignUpScreen from "./screens/signUp";
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+const theme = extendTheme({
+  fontConfig: {
+    Inter: {
+      100: {
+        normal: "Inter_100Thin",
+      },
+      200: {
+        normal: "Inter_200ExtraLight",
+      },
+      300: {
+        normal: "Inter_300Light",
+      },
+      400: {
+        normal: "Inter_400Regular",
+      },
+      500: {
+        normal: "Inter_500Medium",
+      },
+      600: {
+        normal: "Inter_600SemiBold",
+      },
+      700: {
+        normal: "Inter_700Bold",
+      },
+      800: {
+        normal: "Inter_800ExtraBold",
+      },
+      900: {
+        normal: "Inter_900Black",
+      },
+    },
+
+    fonts: {
+      heading: "Inter",
+      body: "Inter",
+      mono: "Inter",
+    },
+  },
+
+
+});
+
+export default function App() {  
+
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="SignInScreen" component={SignInScreen} options={{ headerShown: false, title: "Sign In" }} />
--- screens/signIn/index.tsx
+++ screens/signIn/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
 
 import { VStack, Text, Center, Input, Button, Image, ZStack, Link } from "native-base";
 
@@ -13,41 +14,47 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
 
     const handleLogin = () => {
         if(!email[0] || !password[0]) return
+    }
 
-        console.log(email, password);
+    const [fontsLoaded] = useFonts({
+        'AlfaSlabOne': require('../../assets/fonts/AlfaSlabOne-Regular.ttf'),
+    });
+
+    if(!fontsLoaded) {
+        return(
+            <Text>Loading...</Text>
+        )
     }
 
     return (
         <ZStack height="full" backgroundColor="maroon" justifyContent="center" alignItems="center">
-            <Image source={{
-                    uri: "https://populous.com/wp-content/uploads/2018/01/TEXAFBSTA_0319_ChristyRadecic.jpg"
-                }}
+            <Image source={require("../../assets/kyle.jpg")}
                 alt="Alternate Text" size="full"
                 opacity={0.35}
                 blurRadius={1.5}
             />
 
             <VStack height="full" justifyContent="space-evenly" >
-                <Center>
-                    <Text fontSize="8xl" fontWeight="bold" color="light.50">12th Rec</Text>
-                </Center>  
-                
                 <VStack>
-                    <VStack marginY={2}>
-                        <Text color="light.100" fontWeight="bold" marginY={1}>Email</Text>
-                        <Input value={email} type="email" placeholder="johndoe@gmail.com" color="white" onChangeText={(email) => setEmail(email)} />
-                    </VStack>
+                    <Text fontSize="7xl" fontWeight="bold" color="light.50" style={{ "fontFamily": "AlfaSlabOne", "color": "#F2EDDB" }}>12th Rec</Text>
 
-                    <VStack marginY={2}>
-                        <Text color="light.100" fontWeight="bold" marginY={1}>Password</Text>
-                        <Input value={password} type="password" placeholder="password" color="white" onChangeText={(password) => setPassword(password)}/>
-                    </VStack>
+                    <VStack marginTop={12}>
+                        <VStack marginY={2}>
+                            <Text color="light.100" fontWeight="bold" marginY={1}>Email</Text>
+                            <Input value={email} type="email" placeholder="johndoe@gmail.com" color="white" onChangeText={(email) => setEmail(email)} />
+                        </VStack>
 
-                    <VStack marginY={2}>
-                        <Button onPress={handleLogin} colorScheme="success">Sign in</Button>
-                    </VStack>
-                </VStack> 
+                        <VStack marginY={2}>
+                            <Text color="light.100" fontWeight="bold" marginY={1}>Password</Text>
+                            <Input value={password} type="password" placeholder="password" color="white" onChangeText={(password) => setPassword(password)}/>
+                        </VStack>
 
+                        <VStack marginY={2}>
+                            <Button onPress={handleLogin} colorScheme="success">Sign in</Button>
+                        </VStack>
+                    </VStack> 
+                </VStack>  
+                
                 <Center>
                     <Link onPress={() => navigation.navigate("SignUpSceen")}_text={{ color: "light.50" }} >Don't have an account?</Link>
                 </Center>
