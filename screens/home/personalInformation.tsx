c228bb8b32133ce669dbe4ef02264aa78039c2fc
SumitNalavade <vs.nalavade2003@gmail.com>
2022-09-25 02:27:35 -0500

    Add state to personal info page

--- screens/home/personalInformation.tsx
+++ screens/home/personalInformation.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+
+import useAppStore from "../../store/useAppStore";
 
 import { Button, Heading, Input, InputGroup, InputRightAddon, Text, VStack } from "native-base";
 
 const PersonalInformation: React.FC = () => {
+    const userInfo = useAppStore((state) => state.currentUser);
+
+    const [name, setName] = useState(userInfo.name);
+    const [email, setEmail] = useState(userInfo.email.replace("@tamu.edu", ""));
+    const [classification, setClassification] = useState(userInfo.classification);
+
     return (
         <VStack padding={6} height="1/2" justifyContent="space-between" >
             <VStack>
@@ -13,19 +21,19 @@ const PersonalInformation: React.FC = () => {
             <VStack>
                 <VStack marginY={2}>
                     <Text color="black" fontWeight="bold" marginY={1}>Name</Text>
-                    <Input type="text" placeholder="john doe" color="white" />
+                    <Input value={name} type="text" placeholder="john doe" color="black" onChangeText={(name) => setName(name)} />
                 </VStack>
 
                 <VStack marginY={2} >
                     <Text color="black" fontWeight="bold" marginY={1}>Email</Text>
                     <InputGroup>
-                        <Input width="full" type="email" placeholder="john.doe" color="white" InputRightElement={<InputRightAddon backgroundColor="light.200" children={"@tamu.edu"} />} />
+                        <Input value={email} width="full" type="email" placeholder="john.doe" color="black" onChangeText={(email) => setEmail(email)} InputRightElement={<InputRightAddon backgroundColor="light.200" children={"@tamu.edu"} />} />
                     </InputGroup>
                 </VStack>
 
                 <VStack marginY={2}>
                     <Text color="black" fontWeight="bold" marginY={1}>Classification</Text>
-                    <Input type="text" placeholder="sophomore" color="white" />
+                    <Input value={classification} type="text" placeholder="sophomore" color="black" onChangeText={(classification) => setClassification(classification)} />
                 </VStack>
             </VStack>
 
