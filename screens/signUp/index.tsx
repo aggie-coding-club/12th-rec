72e5330393e479a29dea6541026cb9faa77fd066
SumitNalavade <vs.nalavade2003@gmail.com>
2022-09-24 20:40:48 -0500

    Add confirm password on sign up screen

--- screens/signUp/index.tsx
+++ screens/signUp/index.tsx
@@ -1,9 +1,9 @@
-import React, { useState }  from "react";
+import React, { useState, useEffect }  from "react";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 import { doc, setDoc } from "firebase/firestore"; 
 
-import { VStack, Text, Input, Button, Image, ZStack, Box, InputRightAddon, InputGroup, KeyboardAvoidingView } from "native-base";
+import { VStack, Text, Input, Button, Image, ZStack, Box, InputRightAddon, InputGroup, KeyboardAvoidingView, FormControl, WarningOutlineIcon } from "native-base";
 import { Platform } from "react-native";
 
 import { app, db } from "../../firebase/firebaseConfig";
@@ -17,9 +17,15 @@ const SignUpScreen: React.FC<Props> = ({ navigation }) => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [classification, setClassification] = useState("")
+    const [isInvalid, setIsInvalid] = useState(true)
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    useEffect(() => {
+        confirmPassword === password ? setIsInvalid(false) : setIsInvalid(true)
+    }, [password, confirmPassword])
 
     const handleSignUp = async() => {
-        if(!name || !email || !password || !classification) return 
+        if(!name || !email || !password || !classification || isInvalid) return 
 
         const auth = getAuth(app);
         const user  = await createUserWithEmailAndPassword(auth, `${email}@tamu.edu`, password)
@@ -45,6 +51,7 @@ const SignUpScreen: React.FC<Props> = ({ navigation }) => {
                 <Text fontSize="5xl" fontWeight="bold" color="light.50" style={{ "fontFamily": "AlfaSlabOne", "color": "#F2EDDB" }}>Sign Up</Text>
 
             <KeyboardAvoidingView paddingX={6}  height="full" justifyContent="center"  behavior={Platform.OS === "ios" ? "padding" : "height"}>
+                <FormControl isRequired>
                         <VStack marginY={2}>
                             <Text color="light.100" fontWeight="bold" marginY={1}>Name</Text>
                             <Input value={name} type="text" placeholder="John Doe" color="white" onChangeText={(name) => setName(name)} />
@@ -68,8 +75,17 @@ const SignUpScreen: React.FC<Props> = ({ navigation }) => {
                         </VStack>
 
                         <VStack marginY={2}>
+                            <Text color="light.100" fontWeight="bold" marginY={1}>Confirm Password</Text>
+                            <Input value={confirmPassword} type="password" placeholder="password" color="white" onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)}/>
+                            <FormControl.ErrorMessage isInvalid={isInvalid} leftIcon={<WarningOutlineIcon size="xs" />}>
+                                Passwords must be the same.
+                            </FormControl.ErrorMessage>
+                        </VStack>
+
+                        <VStack marginY={2}>
                             <Button onPress={handleSignUp} colorScheme="success">Sign Up</Button>
                         </VStack>
+                </FormControl>
             </KeyboardAvoidingView>
             </Box>
         </ZStack>
