2165d312f8faa0a79541a95c2437b6da99f68c7d
SumitNalavade <vs.nalavade2003@gmail.com>
2022-10-01 21:03:31 -0500

    Add Image upload

--- screens/settings/index.tsx
+++ screens/settings/index.tsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RouteProp } from '@react-navigation/native';
 import { UserCredential, getAuth } from "firebase/auth"
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 import * as ImagePicker from 'expo-image-picker';
 
 import { Ionicons } from '@expo/vector-icons';
-import { VStack,  Heading, Button, Center, Image } from "native-base";
+import { VStack,  Heading, Button, Center, Image, Box } from "native-base";
 
 import useAppStore from "../../store/useAppStore";
 
@@ -16,33 +17,46 @@ interface Props {
 
 const SettingsScreen: React.FC<Props> = ({ route, navigation }) => {
     const auth = getAuth();
+    const storage = getStorage();
 
-    const [profilePic, setProfilePic] = useState<ImagePicker.ImagePickerResult>();
     const currentUser = useAppStore((state) => state.currentUser)
+    const [profilePicURL, setProfilePic] = useState(`https://firebasestorage.googleapis.com/v0/b/threc-e1518.appspot.com/o/${currentUser.uid}?alt=media&token=`);
 
     const handleLogOut = async () => {
         await auth.signOut();
     }
 
     const uploadImage = async () => {
-        await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.All,
-          allowsEditing: true,
-          aspect: [1, 1],
-          quality: 1,
-        }).then((res) => setProfilePic(res))
-    };
+        const image = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        })
+
+        if(!image["cancelled"]) {
+            const response = await fetch(image["uri"]);
+            const blob = await response.blob();
+            const storageRef = ref(storage, currentUser.uid);
+
+            uploadBytes(storageRef, blob).then((snapshot) => {
+                setProfilePic(`https://firebasestorage.googleapis.com/v0/b/threc-e1518.appspot.com/o/${currentUser.uid}?alt=media&token=`)
+            })
+        }
+    }
 
     return (
         <VStack height="full" width="full" alignItems="center" justifyContent="space-around" safeArea >
             <Center>
-                { profilePic ? (
-                    <Image source={{
-                        uri: "https://wallpaperaccess.com/full/317501.jpg"
-                      }} alt="Alternate Text" size="xl" />
-                ): (
-                    <Ionicons onPress={uploadImage} name="person-circle-outline" size={150} />
-                )  }
+                <Box onTouchEnd={uploadImage} >
+                    { profilePicURL ? (
+                        <Image source={{
+                            uri: profilePicURL
+                        }} alt="Alternate Text" size="xl" />
+                    ): (
+                        <Ionicons name="person-circle-outline" size={150} />
+                    )  }
+                </Box>
                 <Heading fontSize="4xl" color="maroon">{currentUser.name}</Heading>
             </Center>
 
