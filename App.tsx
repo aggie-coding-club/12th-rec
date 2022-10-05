537e7dddbae965e46951580f6f7be97aceca1976
SumitNalavade <vs.nalavade2003@gmail.com>
2022-10-05 17:57:10 -0500

    Add icons to bottom tab navigator

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons"
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -102,7 +103,24 @@ export function AuthStackNavigator() {
 
 export function TabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused
+            ? 'home'
+            : 'home-outline';
+        } else if (route.name === 'Settings') {
+          iconName = focused ? 'person' : 'person-outline';
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: 'maroon',
+      tabBarInactiveTintColor: 'gray',
+    })}>
               <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false  }} />
               <Tab.Screen name="Settings" component={SettingsStackNavigator} options={{ headerShown: false  }} />
       </Tab.Navigator>
