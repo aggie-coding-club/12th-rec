113298c72e023bfbcf3645e605766001017cdeb3
SumitNalavade <vs.nalavade2003@gmail.com>
2022-10-13 04:07:42 -0500

    Render posts

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RouteProp } from '@react-navigation/native';
-import { UserCredential, getAuth } from "firebase/auth"
+import { collection, DocumentData, getDocs } from "firebase/firestore";
 
-import { Text, VStack } from "native-base";
+import { db } from "../../firebase/firebaseConfig";
+
+import { Text, VStack, Box } from "native-base";
+import { IPost } from "../../utils/interfaces";
 
 interface Props {
     navigation: NativeStackNavigationProp<any, any>
-    route: RouteProp<{ params: { user: UserCredential } }, 'params'>
+    route: RouteProp<any>
 }
 
 const HomeScreen: React.FC<Props> = ({ route, navigation }) => {
-    const auth = getAuth();
+    const [posts, setPosts] = useState<DocumentData[]>();
 
-    const handleLogOut = async () => {
-        await auth.signOut();
-    }
+    useEffect(() => {
+        getDocs(collection(db, "posts")).then((res) => {
+            const data = res.docs.map((doc) => doc.data())
+            setPosts(data);
+        })
+    }, [])
 
     return (
         <VStack height="full" safeArea >
-            <Text>Map View</Text>            
+            <Text>Map View</Text> 
+            {posts?.map(post => <Text key={post.title}>{post.title}</Text>)} 
         </VStack>
     )
 }
