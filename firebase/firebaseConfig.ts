cb443dffdf8c18805e336f2682f5e762f06e7bd2
SumitNalavade <vs.nalavade2003@gmail.com>
2022-10-15 20:48:54 -0500

    Add env variables

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -1,3 +1,5 @@
+import { FIREBASEAPIKEY, FIREBASEAUTHDOMAIN , FIREBASEPROJECTID, FIREBASESTORAGEBUCKET, FIREBASEMESSAGINGSENDERID, FIREBASEAPID } from "@env";
+
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
@@ -13,17 +15,17 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyChIiVoGozpH_X953GNWF44R0PJgAi38Tw",
+  apiKey: FIREBASEAPIKEY,
 
-  authDomain: "threc-e1518.firebaseapp.com",
+  authDomain: FIREBASEAUTHDOMAIN,
 
-  projectId: "threc-e1518",
+  projectId: FIREBASEPROJECTID,
 
-  storageBucket: "threc-e1518.appspot.com",
+  storageBucket: FIREBASESTORAGEBUCKET,
 
-  messagingSenderId: "958371623547",
+  messagingSenderId: FIREBASEMESSAGINGSENDERID,
 
-  appId: "1:958371623547:web:d95fa2c421df7efc53a8de"
+  appId: FIREBASEAPID
 
 };
 
