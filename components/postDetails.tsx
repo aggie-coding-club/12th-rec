51828d31325366b18b1ae6ea2a088debf6a53fe1
SumitNalavade <vs.nalavade2003@gmail.com>
2022-11-13 23:50:36 -0600

    Add delete icon to posts

--- components/postDetails.tsx
+++ components/postDetails.tsx
@@ -1,26 +1,41 @@
 import React from "react";
 import DateTimePicker from '@react-native-community/datetimepicker';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, Entypo } from '@expo/vector-icons';
 
 import { IPost } from "../utils/interfaces";
 
-import { Box, VStack, Heading, Text, Flex } from "native-base";
+import { Box, VStack, Heading, Text, Flex, HStack } from "native-base";
+
+import useAppStore from "../store/useAppStore";
 
 interface PostDetails {
   post: IPost;
 }
 
 const PostDetails: React.FC<PostDetails> = ({ post }) => {
+  const currentUser = useAppStore((state) => state.currentUser);
+
   return (
     <Box paddingRight={6} paddingLeft={6} >
-      <VStack>
+      <HStack justifyContent="space-between">
         <Heading size="xl" color="maroon">
           {post.title}
         </Heading>
-        <Text paddingTop={2}>{post.description}</Text>
-      </VStack>
+
+      { currentUser.uid === post.userID ? (
+        <Box paddingTop={0.5}>
+          <Ionicons name="trash" size={24} color="maroon" />
+        </Box>
+      ) : "" }
+
+      </HStack>
 
       <VStack marginY={4}>
+        <Flex marginY={2} flexDirection="row" alignItems="center" >
+          <Entypo name="text" size={24} color="maroon" />
+          <Text paddingLeft={2}>{post.description}</Text>
+        </Flex>
+
         <Flex marginY={2} flexDirection="row" alignItems="center" >
           <Ionicons name="location-outline" size={24} color="maroon" />
           <Text paddingLeft={2}>{post.location}</Text>
